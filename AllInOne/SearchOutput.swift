248818b0686b400e871075f6b2bedc849f87126c
shuan43 <shuan43@emory.edu>
2015-03-19 00:31:42 -0400

    add a class for the search result instead of a string.Demonstrate the simulated result in the table view

---
+++ AllInOne/SearchOutput.swift
@@ -0,0 +1,16 @@
+//
+//  SearchOutput.swift
+//  AllInOne
+//
+//  Created by Sichong Huang on 15/3/18.
+//  Copyright (c) 2015年 Tony. All rights reserved.
+//
+
+import Foundation
+
+class SearchOutput
+{
+    var name = ""
+    var NameOfArtist = ""
+
+}
--- AllInOne/SearchViewController.swift
+++ AllInOne/SearchViewController.swift
@@ -1,63 +1,101 @@
-//
-//  ViewController.swift
-//  AllInOne
-//
-//  Created by Tony on 3/15/15.
-//  Copyright (c) 2015 Tony. All rights reserved.
-//
+
 
 import UIKit
 
 class SearchViewController: UIViewController {
-    var searchResults = [String]()
+    
+
+    var searchOutputs = [SearchOutput]()
+    var hasSearched = false
+    
+    
+
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0,
-            right: 0)
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-@IBOutlet weak var searchBar: UISearchBar!
-@IBOutlet weak var tableView: UITableView!
-
-}
-
-extension SearchViewController: UISearchBarDelegate {
-    func positionForBar(bar: UIBarPositioning) -> UIBarPosition { return .TopAttached
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        
     }
     
-    func searchBarSearchButtonClicked(searchBar: UISearchBar) { searchResults = [String]()
-    for i in 0...5 {
-    searchResults.append(String(format: "This is not the real result  %d for '%@'", i,
-    searchBar.text))
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    tableView.reloadData() }
+    
+    
+    
 }
 
-extension SearchViewController: UITableViewDataSource {
-        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count }
-        
-        
-        func tableView(tableView: UITableView,
-            cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-          let cellIdentifier = "SearchResultCell"
-        
-          var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
-        
-          if cell == nil {
-            cell = UITableViewCell(style: .Default,
-                                    reuseIdentifier: cellIdentifier)
+
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchOutputs = [SearchOutput]()
+        searchBar.resignFirstResponder()
+        hasSearched = true
+        if searchBar.text != "sam smith" {
+            for i in 0...5 {
+                let searchOutput = SearchOutput()
+                searchOutput.name = String(format: "This %d result is not real for", i)
+                searchOutput.NameOfArtist = searchBar.text
+                searchOutputs.append(searchOutput)
+            }
         }
-          
-          cell.textLabel!.text = searchResults[indexPath.row]
-          return cell
-        
-        
+        tableView.reloadData()
+        println("The search text is: '\(searchBar.text)'")
+    }
+    
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
+    }
 }
+
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchOutputs.count == 0 {
+            return 1
+        } else {
+            return searchOutputs.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchOutputCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        if searchOutputs.count == 0 {
+            cell.textLabel!.text = "Nothing matches the database :("
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchOutput = searchOutputs[indexPath.row]
+            cell.textLabel!.text = searchOutput.name
+            cell.detailTextLabel!.text = searchOutput.NameOfArtist
+        }
+        return cell
+    }
 }
-extension SearchViewController: UITableViewDelegate { }
+
+
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if searchOutputs.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
